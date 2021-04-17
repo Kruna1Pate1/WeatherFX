b003b07b7753df0df25ac1da29bfe0b3815f6b12
Kruna1Pate1 <kbpatel5802@gmail.com>
2021-04-18 00:21:05 +0530

    Completed basic working application

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.json.simple.parser.ParseException;
@@ -7,7 +8,10 @@ import org.json.simple.parser.ParseException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Controller of Main.xml 
@@ -17,6 +21,7 @@ public class MainController {
 
     WeatherData data = new WeatherData();
     String city;
+    Image image;
     @FXML
     private Button fetchDataBtn;
 
@@ -24,16 +29,45 @@ public class MainController {
     private TextField cityTextField;
 
     @FXML
+    private ImageView conditionIcon;
+
+    @FXML
+    private Label cityLable, discriptionLable, tempLable, feelLable, rainChanceLable, windSpeedLable, humidityLable, pressureLable;
+
+    private String cityL, discription, temp, feel, rainChance, windSpeed, humidity, pressure, imageUrl;
+    @FXML
     public void fetchData(ActionEvent e) throws IOException, InterruptedException, ParseException {
 
         city = cityTextField.getText();
 
         data = WeatherData.getData(city);
-        System.out.println("City :" + data.getCity());
-        System.out.println("Temprature :" + data.getTemp());
-        System.out.println("Type :" + data.getWeatherType());
-        System.out.println("Description :" + data.getDescription());
-        System.out.println("Code :" + data.getCondition());
 
+        setData(data);
+
+        imageUrl = data.getWeatherIcon();
+        image = new Image(new FileInputStream(imageUrl));
+        conditionIcon.setImage(image);
+    }
+
+    @FXML
+    public void setData(WeatherData data) {
+
+        cityL = data.getCity();
+        discription = data.getDescription();
+        temp = data.getTemp() + "° C";
+        feel = " feels like " + data.getFeelLike() + "° C";
+        rainChance = data.getRain() + "%";
+        windSpeed = data.getWindSpeed() + " m/s";
+        humidity = data.getHumidity() + "%";
+        pressure = data.getPressure() + " hPa";
+
+        cityLable.setText(cityL);
+        discriptionLable.setText(discription);
+        tempLable.setText(temp);
+        feelLable.setText(feel);
+        rainChanceLable.setText(rainChance);
+        windSpeedLable.setText(windSpeed);
+        humidityLable.setText(humidity);
+        pressureLable.setText(pressure);
     }
 }
